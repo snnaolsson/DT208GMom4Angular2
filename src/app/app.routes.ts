ac4ca4c81f4e021e9e58c220b86e17c928efecdc
Sanna Olsson <saol1901@student.miun.se>
2024-05-15 18:39:01 +0200

    updates to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { CoursesComponent } from './courses/courses.component';
 
 export const routes: Routes = [
-    {path: 'courses', component: CoursesComponent}
 ];
